a1743aa819f2fc93b0b986927050e7ff8bbd8f3f
Rezky Aulia Pratama <rezky.pratama@dana.id>
2019-08-16 11:45:56 +0700

    Improve architecture phase 1

---
+++ common/libnavigation/src/main/java/id/innovation/libnavigation/ActivityHelper.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2018 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("ActivityHelper")
+
+package id.innovation.libnavigation
+
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Helpers to start activities in a modularized world.
+ */
+
+private const val PACKAGE_NAME = "id.innovation"
+
+/**
+ * Create an Intent with [Intent.ACTION_VIEW] to an [AddressableActivity].
+ */
+fun intentTo(context: Context, addressableActivity: AddressableActivity): Intent {
+    return Intent(Intent.ACTION_VIEW).setClassName(
+            context.packageName,
+            addressableActivity.className)
+}
+
+/**
+ * An [android.app.Activity] that can be addressed by an intent.
+ */
+interface AddressableActivity {
+    /**
+     * The activity class name.
+     */
+    val className: String
+}
+
+/**
+ * All addressable activities.
+ *
+ * Can contain intent extra names or functions associated with the activity creation.
+ */
+object Activities {
+
+    /**
+     * Example :
+     * MainActivity
+
+    object OnBoardingKyb : AddressableActivity {
+        override val className = "$PACKAGE_NAME.fonboardingkyb.presenter.submission.MainActivity"
+    }
+    */
+
+}
